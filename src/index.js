3239c4581b244330b142c8e36db706a9b3ae88af
Daniel Elward <daniel.elward@hotmail.com>
2025-11-11 14:50:11 -0400

    setup API to pull data from host

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+async function apiRequest(queryLocation){
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${queryLocation}/?key=LB6J72DBNZFB2HL6GXP3Q28CW`);
+    const weatherData = await response.json();
+    
+    return weatherData;
+};
+
+apiRequest("hartland, NB").then(result => {
+    // do things with the result here, like call functions with them
+    console.log(result)
+})
